b86c5fe76e46086762e661470a9182407940e07f
El Mehdi <louihranim@gmail.com>
2023-03-23 22:30:50 +0100

    dash comp spacing

--- src/components/dashboard/DashboardComp.jsx
+++ src/components/dashboard/DashboardComp.jsx
@@ -8,12 +8,12 @@ import Box from "@mui/material/Box";
 const DashboardComp = () => {
   return (
     <Box>
-      <Grid container spacing={3}>
+      <Grid container spacing={2}>
         <Grid item xs={12} lg={8}>
           <SalesOverview />
         </Grid>
         <Grid item xs={12} lg={4}>
-          <Grid container spacing={3}>
+          <Grid container spacing={2}>
             <Grid item xs={12}>
               <YearlyBreakup />
             </Grid>
@@ -22,14 +22,8 @@ const DashboardComp = () => {
             </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={12} lg={4}>
-          Hello
-        </Grid>
         <Grid item xs={12} lg={8}>
           <ProductPerformance />
-        </Grid>
-        <Grid item xs={12}>
-          Hello
         </Grid>
       </Grid>
     </Box>
