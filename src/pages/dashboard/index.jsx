6a369b61380f63f2c34ebb1605e7ddc72356c2ec
El Mehdi <louihranim@gmail.com>
2023-03-23 12:16:13 +0100

    Dashboard

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,7 +1,183 @@
-import React from "react";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Image from "next/image";
+import { RightOutlined } from "@ant-design/icons/lib/icons";
+import { Grid } from "@mui/material";
+import SalesOverview from "@/components/dashboard/SalesOverview";
+import YearlyBreakup from "@/components/dashboard/YearlyBreakup";
+import ProductPerformance from "@/components/dashboard/ProductPerformance";
+import MonthlyEarnings from "@/components/dashboard/MonthlyEarnings";
 
-const index = () => {
-  return <div>index</div>;
-};
+const drawerWidth = 240;
 
-export default index;
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  })
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
+export default function PersistentDrawerLeft() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar color="inherit" position="fixed" open={open}>
+        <Toolbar>
+          <div
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            style={{ marginRight: "20px", ...(open && { display: "none" }) }}
+          >
+            <Image
+              style={{
+                cursor: "pointer",
+              }}
+              alt="menu"
+              width={38}
+              height={7}
+              src="/images/burgerMenu.svg"
+            />
+          </div>
+          <Typography variant="h6" noWrap component="div">
+            Win u Watch Dashboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            <RightOutlined />
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {["Dashboard", "Watches", "Competitions", "Participants"].map(
+            (text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon></ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            )
+          )}
+        </List>
+        <Divider />
+        <List>
+          {["All mail", "Trash", "Spam"].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon></ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+        <Box>
+          <Grid container spacing={3}>
+            <Grid item xs={12} lg={8}>
+              <SalesOverview />
+            </Grid>
+            <Grid item xs={12} lg={4}>
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <YearlyBreakup />
+                </Grid>
+                <Grid item xs={12}>
+                  <MonthlyEarnings />
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={12} lg={4}>
+              Hello
+            </Grid>
+            <Grid item xs={12} lg={8}>
+              <ProductPerformance />
+            </Grid>
+            <Grid item xs={12}>
+              Hello
+            </Grid>
+          </Grid>
+        </Box>
+      </Main>
+    </Box>
+  );
+}
