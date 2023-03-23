3178d184924f4323fc0d73a09160fe285e9d8d49
Iliass JABALI <iliassjabali@gmail.com>
2023-03-23 10:19:10 +0000

    Revert "Abdessalam"

--- src/server/api/routers/index.ts
+++ src/server/api/routers/index.ts
@@ -15,93 +15,6 @@ export const CompetitionRouter = createTRPCRouter({
         },
       });
     }),
-  add: publicProcedure
-    .input(
-      z.object({
-        name: z.string(),
-        start_date: z.date(),
-        end_date: z.date(),
-        remaining_tickets: z.number(),
-        drawing_date: z.string(),
-        total_tickets: z.number(),
-        ticket_price: z.number(),
-        location: z.string(),
-        second_reward: z.string(),
-        status: z.string(),
-        winner: z.string(),
-        watchesId: z.string(),
-      })
-    )
-    .mutation(({ ctx, input }) => {
-      return ctx.prisma.watches.create({
-        data: {
-          name: input.name,
-          start_date: input.start_date,
-          end_date: input.end_date,
-          remaining_tickets: input.remaining_tickets,
-          drawing_date: input.drawing_date,
-          total_tickets: input.total_tickets,
-          ticket_price: input.ticket_price,
-          location: input.location,
-          second_reward: input.second_reward,
-          status: input.status,
-          winner: input.winner,
-          watchesId: input.watchesId,
-        },
-      });
-    }),
-  update: publicProcedure
-    .input(
-      z.object({
-        id: z.string(),
-        name: z.string(),
-        start_date: z.date(),
-        end_date: z.date(),
-        remaining_tickets: z.number(),
-        drawing_date: z.string(),
-        total_tickets: z.number(),
-        ticket_price: z.number(),
-        location: z.string(),
-        second_reward: z.string(),
-        status: z.string(),
-        winner: z.string(),
-        watchesId: z.string(),
-      })
-    )
-    .mutation(({ ctx, input }) => {
-      return ctx.prisma.watches.update({
-        data: {
-          name: input.name,
-          start_date: input.start_date,
-          end_date: input.end_date,
-          remaining_tickets: input.remaining_tickets,
-          drawing_date: input.drawing_date,
-          total_tickets: input.total_tickets,
-          ticket_price: input.ticket_price,
-          location: input.location,
-          second_reward: input.second_reward,
-          status: input.status,
-          winner: input.winner,
-          watchesId: input.watchesId,
-        },
-        where: {
-          id: input.id,
-        },
-      });
-    }),
-  delete: publicProcedure
-    .input(
-      z.object({
-        id: z.string(),
-      })
-    )
-    .mutation(({ ctx, input }) => {
-      return ctx.prisma.watches.delete({
-        where: {
-          id: input.id,
-        },
-      });
-    }),
 });
 
 export const WatchesRouter = createTRPCRouter({
@@ -117,98 +30,24 @@ export const WatchesRouter = createTRPCRouter({
         },
       });
     }),
-  add: publicProcedure
-    .input(
-      z.object({
-        name: z.string(),
-        ImageURL: z.array(z.string()),
-        owner_ref: z.string(),
-        condition: z.string(),
-        years: z.number(),
-        movement: z.string(),
-        case_size: z.number(),
-        dail: z.string(),
-        case_material: z.string(),
-        bracelet_material: z.string(),
-        water_proof: z.string(),
-        box: z.string(),
-        papers: z.string(),
-        price: z.number(),
-      })
-    )
+  add : publicProcedure
+    .input(z.object({ 
+      name: z.string(),
+      price: z.number(),
+      ImageURL : z.array(z.string())
+    }))
     .mutation(({ ctx, input }) => {
       return ctx.prisma.watches.create({
         data: {
           name: input.name,
-          imageURL: input.ImageURL,
-          owner_ref: input.owner_ref,
-          years: input.years,
-          movement: input.movement,
-          case_size: input.case_size,
-          dail: input.dail,
-          case_material: input.case_material,
-          bracelet_material: input.bracelet_material,
-          water_proof: input.water_proof,
-          box: input.box,
-          papers: input.papers,
           price: input.price,
+          imageURL : input.ImageURL
         },
       });
-    }),
-  update: publicProcedure
-    .input(
-      z.object({
-        id: z.string(),
-        name: z.string(),
-        ImageURL: z.array(z.string()),
-        owner_ref: z.string(),
-        condition: z.string(),
-        years: z.number(),
-        movement: z.string(),
-        case_size: z.number(),
-        dail: z.string(),
-        case_material: z.string(),
-        bracelet_material: z.string(),
-        water_proof: z.string(),
-        box: z.string(),
-        papers: z.string(),
-      })
-    )
-    .mutation(({ ctx, input }) => {
-      return ctx.prisma.watches.update({
-        data: {
-          name: input.name,
-          imageURL: input.ImageURL,
-          owner_ref: input.owner_ref,
-          years: input.years,
-          movement: input.movement,
-          case_size: input.case_size,
-          dail: input.dail,
-          case_material: input.case_material,
-          bracelet_material: input.bracelet_material,
-          water_proof: input.water_proof,
-          box: input.box,
-          papers: input.papers
-        },
-        where: {
-          id: input.id,
-        }
-      });
-    }),
-  delete: publicProcedure
-    .input(
-      z.object({
-        id: z.string(),
-      })
-    )
-    .mutation(({ ctx, input }) => {
-      return ctx.prisma.watches.delete({
-        where: {
-          id: input.id,
-        },
-      });
-    }),
+    }
+  ),
 });
+
 
 export const PaymentRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
@@ -232,4 +71,4 @@ export const PaymentRouter = createTRPCRouter({
       }
     }
   ),*/
-});
+})
