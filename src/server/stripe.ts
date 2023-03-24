280332218e4ac5418c80c1e4aff2f8081a4bb20f
Iliass JABALI <iliassjabali@gmail.com>
2023-03-24 22:37:24 +0000

    push

--- src/server/stripe.ts
+++ src/server/stripe.ts
@@ -1,5 +1,10 @@
 // This file will contain the trpc handler for the payment request.
 import { prisma } from "@/server/db";
 import { z } from "zod";
+import { env } from "../env.mjs";
 
-// export const stripeHandler = async (input: {})
+const {STRIPE_SECRET_KEY} = env;
+
+export const stripeHandler =  () => {
+    
+}
