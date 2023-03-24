8f44cc66138ae6594d4c52f9d971971afbd31df7
Iliass JABALI <iliassjabali@gmail.com>
2023-03-24 22:39:05 +0000

    stripe client

--- src/server/stripe.ts
+++ src/server/stripe.ts
@@ -2,9 +2,12 @@
 import { prisma } from "@/server/db";
 import { z } from "zod";
 import { env } from "../env.mjs";
-
+import StripeClient from "stripe";
 const {STRIPE_SECRET_KEY} = env;
 
+const Stripe = new StripeClient(STRIPE_SECRET_KEY, {
+    apiVersion: '2022-11-15',
+});
 export const stripeHandler =  () => {
-    
+
 }
