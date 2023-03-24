1f3064352945d551ce4394847d7414a8fc2896f9
Iliass JABALI <iliassjabali@gmail.com>
2023-03-24 00:58:12 +0000

    Merge branch 'main' into iliass/hook&dbseeding

---
+++ src/components/forms/theme-elements/CustomTextField.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import React from "react";
+import { styled } from "@mui/material/styles";
+import { TextField } from "@mui/material";
+
+const CustomTextField = styled((props: any) => <TextField {...props} />)(
+  ({ theme }) => ({
+    "& .MuiOutlinedInput-input::-webkit-input-placeholder": {
+      color: theme.palette.text.secondary,
+      opacity: "0.8",
+    },
+    "& .MuiOutlinedInput-input.Mui-disabled::-webkit-input-placeholder": {
+      color: theme.palette.text.secondary,
+      opacity: "1",
+    },
+    "& .Mui-disabled .MuiOutlinedInput-notchedOutline": {
+      borderColor: theme.palette.grey[200],
+    },
+  })
+);
+
+export default CustomTextField;
