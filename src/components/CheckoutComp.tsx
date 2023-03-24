fd6f1cf510a92968f89af727b6c30061e40ecbd2
Iliass JABALI <iliassjabali@gmail.com>
2023-03-24 15:52:15 +0000

    minor change

--- src/components/CheckoutComp.tsx
+++ src/components/CheckoutComp.tsx
@@ -50,8 +50,6 @@ const CheckoutComp = () => {
     data
   ) => {
     //check if the date is more than 16 years
-    const LegalAge = 18;
-    const now = new Date();
 
     if (data.date && IsLegal(data.date)) {
       const { sucess, error: trpcError } = await api.Payment.create
