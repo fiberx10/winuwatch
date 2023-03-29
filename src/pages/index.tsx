1489da653feb6ccf590e4056e51bed094504f074
abdessalam staili <abdoycode@gmail.com>
2023-03-29 00:54:18 +0000

    feat : components of our winners

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ import HowToPlay from "@/components/HowToPlay";
 import Certificate from "@/components/Certificate";
 import Reviews from "@/components/Reviews";
 import Footer from "@/components/Footer";
-
+import OurWinner from "@/components/OurWinner";
 import { api } from "@/utils/api";
 
 const Home: NextPage = () => {
@@ -35,6 +35,7 @@ const Home: NextPage = () => {
       <Header />
       <TheCompetition />
       {/* <Winners /> */}
+        <OurWinner/>
       <HowToPlay />
       <Certificate />
       <Reviews />
