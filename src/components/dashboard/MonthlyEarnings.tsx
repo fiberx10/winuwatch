118b6e246e7d90c5ff14405a970337477f5a996a
Iliass JABALI <iliassjabali@gmail.com>
2023-04-01 21:54:03 +0000

    minor

--- src/components/dashboard/MonthlyEarnings.tsx
+++ src/components/dashboard/MonthlyEarnings.tsx
@@ -76,7 +76,7 @@ const MonthlyEarnings = () => {
     >
       <>
         <Typography variant="h3" fontWeight="700" mt="-20px">
-          £6,820
+          £6,82
         </Typography>
         <Stack direction="row" spacing={1} my={1} alignItems="center">
           <Avatar sx={{ bgcolor: errorlight, width: 27, height: 27 }}>
