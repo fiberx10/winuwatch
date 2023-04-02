d6cc593942d264f6de9f4ee80a6ddc3c1771ba1f
El Mehdi <louihranim@gmail.com>
2023-04-03 01:59:09 +0200

    check page

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,18 +1,19 @@
 import { z } from "zod";
-import {OrderSchema} from "./zodSchemas"
+import { OrderSchema } from "./zodSchemas";
 export * from "./api";
 
-export const CreateOrderSchema =  OrderSchema.extend({
+export const CreateOrderSchema = OrderSchema.extend({
   comps: z.array(
     z.object({
       compID: z.string(),
-      quantity: z.number().min(1),
+      number_tickets: z.number().default(1),
+      price_per_ticket: z.number(),
     })
   ),
 }).omit({
   status: true,
   id: true,
-})
+});
 
 export const Formater = (value: number | bigint) =>
   new Intl.NumberFormat("en-UK", {
