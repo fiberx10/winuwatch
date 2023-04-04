7b9d86557ff5fdebe60f00c98fd3d20d0e30e5d8
Fiberx01 <75884799+fiberx10@users.noreply.github.com>
2023-04-04 16:19:59 -0700

    edit page : confirmation page

---
+++ src/pages/Confirmation/[id].tsx
@@ -0,0 +1,47 @@
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { api } from "@/utils/api";
+import styles from "@/styles/Confirmation.module.css";
+import Image from "next/image";
+
+export default function Confirmation() {
+  const { query } = useRouter();
+  const { data } = api.Order.getOrder.useQuery(query.id as string);
+  return (
+    <div>
+      <Head>
+        <title>Order Complete</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <NavBar />
+      <div className={styles.confirmwrapper}>
+        <Image
+          src="/images/confirmation.png"
+          alt="confirmation"
+          className={styles.confirm_icon}
+          width={200}
+          height={200}
+        ></Image>
+        <div className={styles.confirm_text}>
+          <h1>Thank you {data[0]?.first_name} for your order!</h1>
+          <p>
+            Your order has been confirmed . <br /> You will receive an email
+            confirmation shortly.
+          </p>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export async function getServerSideProps(ctx: any) {
+  return {
+    props: {
+      data: null,
+    },
+  };
+}
