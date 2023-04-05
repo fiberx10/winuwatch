e159c0b4cc1b4e53dc391f5cf1c38f780a8aaa2c
El Mehdi <louihranim@gmail.com>
2023-04-05 00:33:03 +0000

    ff

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -34,7 +34,7 @@ export default function Header() {
           muted={true}
         >
           <source
-            src="/images/professional-watches-cosmograph-daytona-cover-video_portrait.mp4"
+            src="https://firebasestorage.googleapis.com/v0/b/winuwatch-bd56d.appspot.com/o/professional-watches-cosmograph-daytona-cover-video_portrait.mp4?alt=media&token=20426bb0-5cee-4a37-8273-6b4fae3f0e95"
             type="video/mp4"
           />
         </video>
--- src/pages/Video.tsx
+++ src/pages/Video.tsx
@@ -4,16 +4,17 @@ const Video = () => {
   return (
     <div
       style={{
-        height: "50vh",
-        position: "fixed",
+        width: "550px",
+        height: "250px",
+        overflow: "hidden",
       }}
     >
-      <video width="100%" height="100%" autoPlay loop playsInline muted={true}>
-        <source
-          src="/images/professional-watches-cosmograph-daytona-cover-video_portrait.mp4"
-          type="video/mp4"
-        />
-      </video>
+      <iframe
+        width="550"
+        height="314"
+        src="https://firebasestorage.googleapis.com/v0/b/winuwatch-bd56d.appspot.com/o/professional-watches-cosmograph-daytona-cover-video_portrait.mp4?alt=media&token=20426bb0-5cee-4a37-8273-6b4fae3f0e95"
+        allowFullScreen
+      ></iframe>
     </div>
   );
 };
