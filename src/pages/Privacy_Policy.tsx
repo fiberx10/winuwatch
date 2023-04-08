e2eb94f2c52802003fb65f04f20d81fc72211475
El Mehdi <louihranim@gmail.com>
2023-04-08 16:08:13 +0000

    Terms Pages

---
+++ src/pages/Privacy_Policy.tsx
@@ -0,0 +1,220 @@
+import Head from "next/head";
+import Link from "next/link";
+import styles from "@/styles/Terms.module.css";
+
+const Privacy_Policy = () => {
+  return (
+    <div className={styles.TermsCon}>
+      <Head>
+        <title>Win u Watch - Privacy Policy</title>
+        <meta name="description" content="Win u Watch - Privacy Policy" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Link href="/">Home</Link>
+      <div className={styles.TermsTxt}>
+        <h1>Privacy Policy</h1>
+        <p className={styles.MainP}>
+          Who are we? <br />
+          <br /> We are Lisam Watch Ltd, trading as Win U Watch, and we are
+          committed to protecting your privacy. If you have any questions about
+          our privacy policy, you can contact us at: contact@winuwatch.uk
+          <br />
+          <br />
+          What is the purpose of this policy? <br />
+          <br />
+          This policy outlines what happens when we collect personal information
+          from you through our website. By using our service, you agree to this
+          policy.
+          <br />
+          <br /> Please note that this policy only applies to the information
+          you provide to us. If you provide personal information to other
+          third-party services, such as payment providers or other websites,
+          please check their respective privacy policies.
+          <br />
+          <br /> Can this policy change?
+          <br />
+          <br /> Yes, we may change this policy from time to time. We encourage
+          you to check this page periodically for any updates. By continuing to
+          use our service, you agree to the updated policy.
+          <br />
+          <br /> What information do we collect?
+          <br />
+          <br /> We may collect the following information from you:
+          <br />
+          <span className={styles.span}>
+            <p>
+              &bull; Information you upload to our service, such as your name
+              and contact details, details about your transactions on our
+              service, contact or other information which you give or allow us
+              to use for newsletters or other marketing, and your communications
+              with us.
+            </p>
+            <br />
+            <p>
+              &bull; Automated information about your use of our service, such
+              as the internet protocol (IP) address used to connect your device
+              to the internet, connection information such as browser type and
+              version, information about your device including device-type and
+              device identifier, operating system and platform, mobile network
+              data, a unique reference number linked to the data you enter on
+              our system, login details, the site from which you arrived at our
+              service, details of your activity with date/time stamps including
+              pages you visited and your searches/transactions.
+            </p>
+            <br />
+            <p>
+              &bull; The following information about you may be provided to us
+              by other people: limited billing information sent to us by our
+              payment provider for verification purposes, such as your name,
+              email address, and billing address.
+            </p>
+          </span>
+          <br /> Why do we collect this information?
+          <br />
+          <br /> We collect this information for the following reasons:
+          <br />
+          <span className={styles.span}>
+            <p>
+              &bull; To take steps at your request to enter into a contract with
+              you and/or to perform such a contract, such as sending service
+              messages, processing payments, and fulfilling orders.
+            </p>
+            <br />
+            <p>
+              &bull; To manage and improve our service, including tracking usage
+              patterns and preventing or detecting fraud or abuse.
+            </p>
+            <br />
+            <p>
+              &bull; With your permission, to send you newsletters or other
+              marketing materials.
+            </p>
+            <br />
+            <p>
+              &bull; If you supply us with someone else&apos;s email address for
+              a &quot;Refer A Friend&quot; or similar offer, to send a referral
+              message to that person.
+            </p>
+          </span>
+          <br />
+          <br />
+          How long do we keep personal information? <br />
+          <br /> We keep personal information for 12 months or until your
+          account is closed, if sooner. You can close your account by emailing
+          us at the above email address.
+          <br />
+          <br /> We may hold on to some of your information for longer,
+          typically up to six years, if reasonably needed for legal, regulatory,
+          or tax reasons, to deal with disputes, prevent fraud or abuse, and/or
+          enforce our terms and conditions.
+          <br />
+          <br /> To whom do we send or make available your personal information?
+          <br />
+          <br />
+          We may share your personal information with the following third-party
+          service providers:
+          <br />
+          <span className={styles.span}>
+            <p>
+              &bull; Delivery companies, website hosts, payment providers,
+              businesses which help us send communications or monitor our
+              website and which provide us with e-commerce, analytics, and other
+              IT services.
+            </p>
+            <br />
+            <p>
+              &bull; Law enforcement authorities to help deal with fraud and
+              abuse and/or comply with legal requirements.
+            </p>
+            <br />
+            <p>
+              &bull; Insurers and professional advisers in connection with our
+              insurance cover or to deal with legal claims.
+            </p>
+            <br />
+            <p>
+              &bull; Potential buyers in the case of an actual or proposed
+              (including negotiations for a) sale or merger or business
+              combination involving all or the relevant part of our business.
+            </p>
+          </span>
+          <br />
+          Do we send your information outside the European Union? <br /> <br />
+          Your personal information which we collect is stored within the EU and
+          is not transferred to any third countries except as follows.
+          <br /> <br /> Your personal information may be transferred to the US
+          by the following companies certified under the{" "}
+          <a href="https://ec.europa.eu/info/law/law-topic/data-protection/data-transfers-outside-eu/eu-us-privacy-shield_en">
+            EU-US Privacy Shield Framework
+          </a>
+          , which provides certain safeguards for your personal information:
+          Google (analytics), Facebook (advertising), Mailchimp and Mandrill
+          (email delivery).
+          <br /> <br /> What about cookies?
+          <br /> <br /> We and/or other companies use cookies and other tracking
+          technologies on our website. Cookies are identifiers (small files of
+          letters and numbers) that are sent to your web browser. Cookies are
+          widely used to make websites work, or work more efficiently, as well
+          as to provide information to the website owner or others. Some are
+          session temporary &apos;session&apos; cookies that remain in the
+          cookie file of your browser only until your browser is closed. Whereas
+          persistent cookies stay for longer (depending on the lifetime of the
+          specific cookie).
+          <br /> <br /> For further information on cookies, including how to use
+          your browser to block them and how to delete existing cookies, you can
+          click here. Note that if you configure your browser to reject cookies,
+          our site&apos;s functionality will be limited.
+          <br /> <br /> Companies which provide us with a service also place
+          cookies. Some of these cookies (e.g., from Google) may involve certain
+          information, such as your IP address and web address of the page
+          you’re visiting, being sent to the company concerned. Below is a
+          summary of the kinds of cookies used together with details about who
+          places them and where you can go to get more information and to opt
+          out (where possible):
+          <br /> <br /> How do analytics cookies work? Analytics cookies help us
+          track the number of visitors to our website and provide us with
+          information about their visits. This information includes the duration
+          of their visit, the pages they viewed, and where they came from. This
+          helps us improve our website and ensure that users can find what they
+          need easily. These cookies are provided by Google Analytics.
+          <br /> <br /> What are advertising cookies used for? Advertising
+          cookies are used to personalize ads on our site and other sites based
+          on your use of our site. They also help measure ad effectiveness, such
+          as how often you click on or view ads and whether you make a purchase
+          from the advertiser. These cookies are provided by Google and
+          Facebook.
+          <br /> <br /> What are payment provider cookies? Payment provider
+          cookies are placed by our payment provider, Stripe, if you use their
+          payment services on our site.
+          <br /> <br /> These cookies help ensure that your payment transactions
+          are secure and that our payment system functions properly.
+          <br /> <br /> What rights do you have? If the legal requirements are
+          met, you have the right to ask us for access to your personal
+          information, to rectify it if there are mistakes, to delete or
+          restrict its use in certain circumstances, or to &apos;data
+          portability&apos; or to withdraw any consent you&spos;ve given (e.g.
+          marketing). You may also have the right to object to the use of your
+          personal information in certain circumstances.
+          <br /> <br /> If you have a complaint about how we are dealing with
+          your personal information, please contact us via the email address
+          above. If you are not happy with our response or think we are not
+          handling your personal information in accordance with the law, you
+          have the right to complain to the Information Commissioner&apos;s
+          Office (ICO).
+          <br /> <br /> For more information about your rights, visit the
+          ICO&apos;s website.
+          <br /> <br /> Contact us If you have any questions or concerns about
+          this privacy policy or our use of your personal information, please
+          contact us via email at contact@winuwatch.uk or write to us at the
+          address listed on our website.
+          <br /> <br /> Effective date This privacy policy is effective as of
+          1st April 2023.
+          <br /> <br />
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Privacy_Policy;
