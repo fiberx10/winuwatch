5b925d4df7af895df34a46cf53b2ba1597591436
El Mehdi <74713797+Mehdi01-git@users.noreply.github.com>
2023-04-08 01:47:05 +0000

    Update Header.tsx video test maymana

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -35,7 +35,7 @@ export default function Header() {
           muted={muted}
         >
           <source
-            src="https://firebasestorage.googleapis.com/v0/b/winuwatch-bd56d.appspot.com/o/professional-watches-cosmograph-daytona-cover-video_portrait.mp4?alt=media&token=20426bb0-5cee-4a37-8273-6b4fae3f0e95"
+            src="https://maymana.ma/spot.mp4"
             type="video/mp4"
           />
         </video>
