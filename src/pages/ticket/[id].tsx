d405060c1b2ff423de69220d93caeedc90c8a687
Fiberx01 <75884799+fiberx10@users.noreply.github.com>
2023-04-08 15:34:43 +0000

    design page : /ticket/[id]

--- src/pages/ticket/[id].tsx
+++ src/pages/ticket/[id].tsx
@@ -3,7 +3,7 @@ import NavBar from "@/components/NavBar";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { api } from "@/utils/api";
-import styles from "@/styles/Confirmation.module.css";
+import styles from "@/styles/Ticket.module.css";
 import { z } from "zod";
 import QRCode from "react-qr-code";
 
@@ -25,6 +25,8 @@ export default function Confirmation({
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const { data } = api.Tickets.getTicket.useQuery(id);
 
+  console.log(JSON.stringify(data));
+
   return (
     <div>
       <Head>
@@ -33,18 +35,31 @@ export default function Confirmation({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavBar />
-      <div className={styles.confirmwrapper}></div>
       <div className={styles.confirmwrapper}>
         {data && (
-          <div className={styles.confirm_text}>
-            <h1>Ticket : {data.id}</h1>
-            <QRCode
-              size={50}
-              style={{ height: "auto", maxWidth: "10%", width: "10%" }}
-              value={data.id}
-              viewBox={`0 0 50 50`}
-            />
-            <p>{JSON.stringify(data)}</p>
+          <div className={styles.wrapper}>
+            <div className={styles.wrapper_left}>
+              <div className={styles.comp_info}>
+                <h6>{data.Competition.name}</h6>
+                <h5>{data.Competition.drawing_date.toDateString()}</h5>
+              </div>
+
+              <div className={styles.ticket_info}>
+                <h6>
+                  Customer Name : {data.Order.first_name} {data.Order.last_name}
+                </h6>
+                <h6> Total Price : {data.Order.totalPrice} $ </h6>
+              </div>
+            </div>
+            <div className={styles.wrapper_right}>
+              <QRCode
+                size={50}
+                style={{ height: "auto", maxWidth: "40%", width: "40%" }}
+                value={data.id}
+                viewBox={`0 0 50 50`}
+              />
+              <h6>Ticket Number : {data.id}</h6>
+            </div>
           </div>
         )}
       </div>
---
+++ src/server/index.ts
@@ -0,0 +1,63 @@
+import { render } from "@react-email/render";
+import Email from "@/components/emails";
+import nodemailer from "nodemailer";
+import { faker } from "@faker-js/faker";
+import { Order, OrderStatus, PaymentMethod, Ticket } from "@prisma/client";
+const GENorder = (): Order & {
+  Ticket: Ticket[];
+} => ({
+  id: faker.datatype.uuid(),
+  first_name: faker.name.firstName(),
+  last_name: faker.name.lastName(),
+  country: faker.address.country(),
+  address: faker.address.streetAddress(),
+  town: faker.address.city(),
+  zip: faker.address.zipCode(),
+  email: faker.internet.email(),
+  phone: faker.phone.number(),
+  date: faker.date.past(),
+  status: faker.helpers.arrayElement([
+    OrderStatus.PENDING,
+    OrderStatus.CONFIRMED,
+    OrderStatus.CANCELLED,
+  ]),
+  paymentMethod: faker.helpers.arrayElement([
+    PaymentMethod.PAYPAL,
+    PaymentMethod.STRIPE,
+  ]),
+  checkedEmail: faker.datatype.boolean(),
+  checkedTerms: faker.datatype.boolean(),
+  totalPrice: faker.datatype.number(1000),
+  paymentId: faker.datatype.uuid(),
+  createdAt: faker.date.past(),
+  updatedAt: faker.date.past(),
+  Ticket: new Array(faker.datatype.number(10)).fill(0).map((_) => ({
+    id: faker.datatype.uuid(),
+    orderId: faker.datatype.uuid(),
+    competitionId: faker.datatype.uuid(),
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.past(),
+  })),
+});
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.hostinger.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "noreply@winuwatch.uk",
+    pass: "Password1!",
+  },
+});
+async function send() {
+  //  res.send("Order Confirmation")));
+  await transporter.sendMail({
+    from: "noreply@winuwatch.uk",
+    to: "fiberxsch108@gmail.com",
+    subject: "Order Confirmation",
+    html: render(Email(GENorder())),
+  });
+  //res.send(render(Email(GENorder())));
+}
+
+export { send };
