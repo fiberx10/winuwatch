edc428b446d21ee591433a0bae6d3c8afc5da218
El Mehdi <louihranim@gmail.com>
2023-04-09 20:06:52 +0000

    video testing

--- src/pages/Video.tsx
+++ src/pages/Video.tsx
@@ -5,7 +5,19 @@ const Video = () => {
     <div className={styles.vid}>
       <div className={styles.headerback}></div>
       <video width="100%" height="100%" autoPlay loop playsInline muted={true}>
-        <source src="https://maymana.ma/spot.mp4" type="video/mp4" />
+        {/* <source
+            src="/images/professional-watches-cosmograph-daytona-cover-video_portrait.webm"
+            type="video/webm"
+          /> */}
+        {/* <source
+          src="/images/professional-watches-cosmograph-daytona-cover-video_portrait.mp4"
+          type="video/mp4"
+        /> */}
+        <source
+          src="/images/professional-watches-cosmograph-daytona-cover-video_portrait.mov"
+          type="video/quicktime"
+        />
+        Your browser does not support the video tag.
       </video>
     </div>
   );
