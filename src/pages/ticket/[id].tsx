c6ce0b76aeaf14932d723aba81e80b2be68d7e05
Fiberx01 <75884799+fiberx10@users.noreply.github.com>
2023-04-09 23:56:44 +0000

    edit : ticket page

--- src/pages/ticket/[id].tsx
+++ src/pages/ticket/[id].tsx
@@ -5,6 +5,7 @@ import { api } from "@/utils/api";
 import styles from "@/styles/Ticket.module.css";
 import { z } from "zod";
 import QRCode from "react-qr-code";
+
 import type {
   GetServerSidePropsContext,
   InferGetServerSidePropsType,
@@ -27,19 +28,19 @@ export default function Confirmation({
       <div className={styles.confirmwrapper}>
         {data && (
           <div className={styles.wrapper}>
-            <h1>Your Ticket Code : {data.id.slice(0, 8)}...</h1>
+            <h1>Your Ticket Code : {data.id}</h1>
 
             <div className={styles.head}>
               <header>
                 <p>Details</p>
-                <p>Order {data.Order.id.slice(0, 8)}... </p>
+                <p>Order {data.Order.id} </p>
               </header>
               <div className={styles.comp_info}>
                 <h6>{data.Competition.name}</h6>
               </div>
               <header>
                 <p>Value of this ticket</p>
-                <p> £ {data.Order.totalPrice} </p>
+                <p> £ {data.Competition.ticket_price} </p>
               </header>
             </div>
 
