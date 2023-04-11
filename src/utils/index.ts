4d977314b3f4726d0a415aa517285237cfe1262a
El Mehdi <louihranim@gmail.com>
2023-04-11 18:00:38 +0000

    london GMT

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,6 @@ import { z } from "zod";
 import { OrderSchema } from "./zodSchemas";
 export * from "./api";
 
-
 export const CreateOrderSchema = OrderSchema.extend({
   comps: z.array(
     z.object({
@@ -26,8 +25,8 @@ export const DateFormater = (value: Date) =>
   new Intl.DateTimeFormat(LOCAL, {
     dateStyle: "full",
     timeStyle: "long",
+    timeZone: "Europe/London",
   }).format(value);
-
 
 export const MAX_TICKETS = 25;
 export const TICKETREDUC = [
