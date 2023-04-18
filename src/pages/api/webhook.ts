7a3c4246ec376b6b7fb0c645383e43760cb09836
Iliass JABALI <iliassjabali@gmail.com>
2023-04-18 00:01:10 +0000

    tmp

---
+++ src/pages/api/webhook.ts
@@ -0,0 +1,108 @@
+import { env } from "@/env.mjs";
+import { prisma } from "@/server/db";
+import type { NextApiRequest, NextApiResponse } from "next";
+import rawBody from "raw-body";
+import Stripe from "stripe";
+import nodemailer from "nodemailer";
+import Email from "@/components/emails";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
+  apiVersion: "2022-11-15",
+});
+
+const Transporter = nodemailer.createTransport({
+  host: "smtp.hostinger.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: env.SMTP_USER,
+    pass: env.SMTP_PASSWORD,
+  },
+});
+
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method === "POST") {
+    const body = await rawBody(request);
+    const signature = request.headers["stripe-signature"] as string;
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        body,
+        signature,
+        env.STRIPE_ENDPOINT_SECRET
+      );
+    } catch (error) {
+      return (
+        console.error(`⚠️ Webhook signature verification failed.`, error),
+        response
+          .status(400)
+          .send(`Webhook Error`)
+      );
+    }
+    switch (event.type) {
+        
+      case "checkout.session.completed":
+        const checkoutSessionCompleted = event.data.object;
+        // const {id,payment_intent } = checkoutSessionCompleted;
+        // Then define and call a function to handle the event payment_intent.succeeded
+        break;
+      case "payment_intent.succeeded":
+        const paymentIntentSucceeded = event.data.object;
+        // const {id } = paymentIntentSucceeded;
+        // Then define and call a function to handle the event payment_intent.succeeded
+        console.log(JSON.stringify(paymentIntentSucceeded, null, 2));
+
+        /*
+        await prisma.order.updateMany({
+          where: {
+            //@ts-ignore
+            intentId: paymentIntentSucceeded.id as string,
+          },
+          //@ts-ignore
+          data: {
+            //@ts-ignore
+            status: "CONFIRMED",
+          },
+        });
+        const dataUp = await prisma.order.findMany({
+          where: {
+            //@ts-ignore
+            intentId: paymentIntentSucceeded.id as string,
+          },
+          include: {
+            Ticket: true,
+            Competition: {
+              include: {
+                Watches: {
+                  include: {
+                    images_url: true,
+                  },
+                },
+              },
+            },
+          },
+        });
+
+        await Transporter.sendMail({
+          from: "noreply@winuwatch.uk",
+          to: dataUp.email,
+          subject: `Order Confirmation - Winuwatch #${dataUp?.id || "000000"}`,
+          html: Email(dataUp),
+        });*/
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+    console.log(event.data);
+    return response.json({});
+  }
+};
