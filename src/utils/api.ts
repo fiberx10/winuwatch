f4f1576b080eb0077c534c268befca941d2c6430
Iliass JABALI <iliassjabali@gmail.com>
2023-04-19 02:17:20 +0000

    push

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -9,9 +9,10 @@ import { createTRPCNext } from "@trpc/next";
 import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
 import superjson from "superjson";
 import { type AppRouter } from "@/server/api/root";
+import {env} from "../env.mjs";
 export const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.HOST) return process.env.HOST; // SSR should use vercel url
+  if (env.HOST) return env.HOST; // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
