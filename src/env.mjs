ad5611c3a5ce8e489b3d51299798930ca5919791
Iliass JABALI <iliassjabali@gmail.com>
2023-04-19 00:20:44 +0000

    fixed

--- src/env.mjs
+++ src/env.mjs
@@ -45,7 +45,7 @@ const processEnv = {
   SMTP_PORT: process.env.SMTP_PORT,
   SMTP_USER: process.env.SMTP_USER,
   SMTP_PASSWORD: process.env.SMTP_PASSWORD,
-  HOST: process.env.HOST,
+  HOST: process.env.NEXTAUTH_SECRET,
 };
 
 // Don't touch the part below
