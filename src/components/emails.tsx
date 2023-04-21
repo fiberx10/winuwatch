76c4f2fb6dedaf5468404713f17b96dbfa29e3e7
El Mehdi <louihranim@gmail.com>
2023-04-21 17:55:39 +0000

    emails layout

--- src/components/emails.tsx
+++ src/components/emails.tsx
@@ -1,6 +1,4 @@
-import type {
-  PrismaClient,
-} from "@prisma/client";
+import type { PrismaClient } from "@prisma/client";
 import React from "react";
 import { Formater, getBaseUrl, DateFormater } from "@/utils";
 
@@ -131,28 +129,29 @@ export const Email = ([order, comps]: [
                         processed. you have now officially entered in the{" "}
                       </p>
                       <p style={{ margin: "0" }}>
-                        {
-                          comps.length > 1
-                            ? "competitions"
-                            : "competition"
-                        }
-                        </p>
+                        {comps.length > 1 ? "competitions" : "competition"}
+                      </p>
                     </div>
                   </td>
                 </tr>
               </tbody>
             </table>
-            {
-              comps.map((compo) => (
-                <>
-                
-                </>
-                ))
-            }
-            <table align="center" width="100%" border={0}>
-              <tbody>
-                <tr>
-                  {comps.map((c) => (
+            {comps.map((c, i) => (
+              <table
+                style={{
+                  maxWidth: "37.5em",
+                  margin: "0 auto",
+                  fontFamily: "Kanit, sans-serif",
+                  textAlign: "center",
+                  fontWeight: "500",
+                }}
+                key={i}
+                align="center"
+                width="100%"
+                border={0}
+              >
+                <tbody>
+                  <tr>
                     <td>
                       <img
                         alt={c.id}
@@ -252,127 +251,225 @@ export const Email = ([order, comps]: [
                         </tbody>
                       </table>
                     </td>
-                  ))}
-                </tr>
-              </tbody>
-            </table>
-
-            <table
-              align="center"
-              width="100%"
-              border={0}
-              cellPadding={0}
-              cellSpacing={0}
-              role="presentation"
-            >
-              <tbody>
-                <tr>
-                  <td>
-                    <table
-                      align="center"
-                      width="100%"
-                      style={{
-                        textAlign: "left",
-                        textTransform: "uppercase",
-                        backgroundColor: "black",
-                        color: "white",
-                      }}
-                      border={0}
-                      cellPadding={0}
-                      cellSpacing={0}
-                      role="presentation"
-                    >
-                      <tbody>
-                        <tr>
-                          <td
-                            style={{
-                              fontSize: "16px",
-                              flex: "1",
-                              textAlign: "left",
-                              lineHeight: "24px",
-                              margin: "0px",
-                              padding: "10px",
-                              paddingLeft: "20px",
-                              textTransform: "uppercase",
-                              color: "white",
-                              backgroundColor: "black",
-                            }}
-                          >
-                            My Tickets
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-
-            <table
-              style={{
-                padding: "0px 0px 0px 0px",
-                width: "100%",
-                paddingLeft: "20px",
-                minWidth: "100%",
-                border: "1px solid rgb(146, 124, 102)",
-              }}
-            >
-              <tbody>
-                {comps.map(({ Ticket }) =>
-                  Ticket.map((ticket, index) => (
-                    <tr
-                      key={index}
-                      style={{
-                        width: "100%",
-                        border: "1px solid rgb(146, 124, 102)",
-                      }}
-                    >
+                  </tr>
+                </tbody>
+                <table
+                  align="center"
+                  width="100%"
+                  border={0}
+                  cellPadding={0}
+                  cellSpacing={0}
+                  role="presentation"
+                  style={{
+                    maxWidth: "37.5em",
+                    margin: "0 auto",
+                    fontFamily: "Kanit, sans-serif",
+                    textAlign: "center",
+                    fontWeight: "500",
+                  }}
+                >
+                  <tbody>
+                    <tr>
                       <td>
-                        <p
+                        <table
+                          align="center"
+                          width="100%"
                           style={{
-                            fontSize: "13px",
-                            lineHeight: "24px",
-                            paddingRight: "5px",
+                            textAlign: "left",
                             textTransform: "uppercase",
-                            color: "black",
+                            backgroundColor: "black",
+                            color: "white",
                           }}
+                          border={0}
+                          cellPadding={0}
+                          cellSpacing={0}
+                          role="presentation"
                         >
-                          {index + 1}
-                        </p>
-                      </td>
-                      <td>
-                        <p
-                          style={{
-                            fontSize: "13px",
-                            lineHeight: "24px",
-                            textTransform: "uppercase",
-                            color: "black",
-                          }}
-                        >
-                          {ticket.id}
-                        </p>
-                      </td>
-                      <td>
-                        <a
-                          style={{
-                            fontSize: "13px",
-                            textAlign: "end",
-                            lineHeight: "24px",
-                            textTransform: "uppercase",
-                            color: "black",
-                            textDecoration: "underline",
-                          }}
-                          href={`${getBaseUrl()}/ticket/${ticket?.id}`}
-                        >
-                          see my ticket
-                        </a>
+                          <tbody>
+                            <tr>
+                              <td
+                                style={{
+                                  fontSize: "16px",
+                                  flex: "1",
+                                  textAlign: "left",
+                                  lineHeight: "24px",
+                                  margin: "0px",
+                                  padding: "10px",
+                                  paddingLeft: "20px",
+                                  textTransform: "uppercase",
+                                  color: "white",
+                                  backgroundColor: "black",
+                                }}
+                              >
+                                My Tickets
+                              </td>
+                              <table
+                                style={{
+                                  padding: "0px 0px 0px 0px",
+                                  paddingLeft: "20px",
+                                  width: "100%",
+                                  fontFamily: "Kanit, sans-serif",
+                                  border: "1px solid rgb(146, 124, 102)",
+                                }}
+                              >
+                                <tbody>
+                                  {c.Ticket.map((ticket, index) => (
+                                    <tr
+                                      key={index}
+                                      style={{
+                                        width: "100%",
+                                        border: "1px solid rgb(146, 124, 102)",
+                                      }}
+                                    >
+                                      <td>
+                                        <p
+                                          style={{
+                                            fontSize: "13px",
+                                            lineHeight: "24px",
+                                            paddingRight: "5px",
+                                            textTransform: "uppercase",
+                                            color: "black",
+                                          }}
+                                        >
+                                          {index + 1}
+                                        </p>
+                                      </td>
+                                      <td>
+                                        <p
+                                          style={{
+                                            fontSize: "13px",
+                                            lineHeight: "24px",
+                                            textTransform: "uppercase",
+                                            color: "black",
+                                          }}
+                                        >
+                                          {ticket.id}
+                                        </p>
+                                      </td>
+                                      <td>
+                                        <a
+                                          style={{
+                                            fontSize: "13px",
+                                            textAlign: "end",
+                                            lineHeight: "24px",
+                                            textTransform: "uppercase",
+                                            color: "black",
+                                            textDecoration: "underline",
+                                          }}
+                                          href={`${getBaseUrl()}/ticket/${
+                                            ticket?.id
+                                          }`}
+                                        >
+                                          see my ticket
+                                        </a>
+                                      </td>
+                                    </tr>
+                                  ))}
+                                </tbody>
+                              </table>
+                              <table
+                                align="center"
+                                width="100%"
+                                style={{ fontWeight: 500 }}
+                                border={0}
+                                cellPadding={0}
+                                cellSpacing={0}
+                                role="presentation"
+                              >
+                                <tbody>
+                                  <tr>
+                                    <td>
+                                      <p
+                                        style={{
+                                          fontSize: "16px",
+                                          lineHeight: "24px",
+                                          margin: "5px 0px",
+                                          textTransform: "uppercase",
+                                          color: "black",
+                                          textAlign: "center",
+                                          letterSpacing: "0.05rem",
+                                          padding: "20px",
+                                        }}
+                                      >
+                                        What happens now?
+                                        <br /> the contest will end on{" "}
+                                        {DateFormater(c.end_date) +
+                                          " (Local Time in London) "}
+                                        , the winners will be announced on{" "}
+                                        {DateFormater(c.drawing_date) +
+                                          " (Local Time in London) "}
+                                        <br /> please make sure to follow us on
+                                        instagram where we <br />
+                                        host a live for each competition. <br />{" "}
+                                        we wish you the best of luck!
+                                      </p>
+                                      <a
+                                        style={{
+                                          width: "100%",
+                                          display: "flex",
+                                          justifyContent: "center",
+                                          margin: "0  auto",
+                                        }}
+                                        href="https://www.instagram.com/winuwatch/"
+                                      >
+                                        <img
+                                          alt="instaFollow"
+                                          src="https://www.win-u-watch.uk/images/InstaFooter.png"
+                                          style={{ margin: "0  auto" }}
+                                        />
+                                      </a>
+                                      <h4
+                                        style={{
+                                          width: "100%",
+                                          textAlign: "center",
+                                          fontWeight: "400",
+                                          background: "lightgrey",
+                                        }}
+                                      >
+                                        Lisam Watch Ltd 63-66 Hatton Gardens,
+                                        London, EC1N 8LE, UK
+                                      </h4>
+                                      <div style={{ display: "flex" }}>
+                                        <h5
+                                          style={{
+                                            margin: "0",
+                                            fontWeight: "300",
+                                            marginRight: "auto",
+                                          }}
+                                        >
+                                          NO LONGER WANT TO RECEIVE THESE
+                                          EMAILS? UNSUBSCRIBE
+                                        </h5>
+                                        <a
+                                          style={{
+                                            textDecoration: "none",
+                                            color: "black",
+                                          }}
+                                          href="https://www.win-u-watch.uk/Privacy_Policy"
+                                        >
+                                          <h5
+                                            style={{
+                                              margin: "0",
+                                              fontWeight: "300",
+                                            }}
+                                          >
+                                            PRIVACY POLICY
+                                          </h5>
+                                        </a>
+                                      </div>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </tr>
+                          </tbody>
+                        </table>
                       </td>
                     </tr>
-                  ))
-                )}
-              </tbody>
-            </table>
-
+                  </tbody>
+                </table>
+              </table>
+            ))}
             <table
               align="center"
               width="100%"
@@ -382,9 +479,7 @@ export const Email = ([order, comps]: [
               cellSpacing={0}
               role="presentation"
             >
-              <tbody>
-                          
-              </tbody>
+              <tbody></tbody>
             </table>
           </td>
         </tr>
