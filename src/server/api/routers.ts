724a5f18694da3f5180ed97a1483ccb20980a79c
Meriem Barhoumi <barhoumi.meriem1@gmail.com>
2023-05-07 21:35:48 +0100

    feat: affiliation fixed amount

--- src/server/api/routers.ts
+++ src/server/api/routers.ts
@@ -1585,7 +1585,7 @@ export const AffiliationRouter = createTRPCRouter({
         discountRate: z.number().gte(0).lte(100).optional(),
         discountAmount: z.number().gte(0).optional(),
         ownerEmail: z.string().email().optional(),
-        compitionId: z.string().optional(),
+        competitionId: z.string().optional(),
       })
     )
     .mutation(async ({ ctx, input }) => {
@@ -1595,15 +1595,19 @@ export const AffiliationRouter = createTRPCRouter({
           data.discountRate = data.discountRate / 100;
         }
         if (data.ownerEmail) {
+          console.log(data);
+
           const hasAffiliation = await ctx.prisma.affiliation.findFirst({
             where: {
-              competitionId: data.compitionId,
+              competitionId: data.competitionId,
               ownerEmail: data.ownerEmail,
               id: {
                 not: id,
               },
             },
           });
+          console.log(hasAffiliation);
+
           if (hasAffiliation) {
             throw new TRPCError({
               code: "BAD_REQUEST",
