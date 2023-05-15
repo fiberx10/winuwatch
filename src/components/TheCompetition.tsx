da8adaa661eadceb66386ab2dbc4972906f744b5
El Mehdi <louihranim@gmail.com>
2023-05-15 11:06:19 +0100

    fixed message under competition

--- src/components/TheCompetition.tsx
+++ src/components/TheCompetition.tsx
@@ -56,25 +56,24 @@ const TheCompetition = () => {
                   <div className={styles.watchContent}>
                     <Link href={`/Competition/${watch.id}`}>{t("start")}</Link>
                     <h3>{watch.name}</h3>
-                    <p>{watch.total_tickets}{" "+t("maxTickets")}</p>
-                    {
-                    /* watch.end_date < new Date() ? (
-                      ""
+                    {/* <p>{watch.total_tickets}{" "+t("maxTickets")}</p> */}
+
+                    {/* // If the end date is less than 1 day away, then show xxx */}
+
+                    {Math.floor(
+                      (new Date(watch.end_date).getTime() -
+                        new Date().getTime()) /
+                        (1000 * 60 * 60 * 24)
+                    ) < 2 ? (
+                      <p>
+                        {t("only")} {watch.remaining_tickets} {t("tickets")}
+                      </p>
                     ) : (
                       <p>
-                        {t("only")} <b>{
-                        // If the end date is less than 1 day away, then show xxx
-                          /*
-                          Math.floor(
-                            (new Date(watch.end_date).getTime() - new Date().getTime()) /
-                              (1000 * 60 * 60 * 24)
-                          ) < 2
-                            ? "xxx"
-                            : watch.remaining_tickets
-                        }</b> {t("tickets")}
+                        {watch.total_tickets}
+                        {" " + t("maxTickets")}
                       </p>
-                      )*/}
-                   
+                    )}
                   </div>
                 </div>
                 <Timer displayFlex={false} date={watch.end_date} />
