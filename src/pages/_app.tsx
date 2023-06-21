4932434c289e334b0d6ed144c8e8c0cc36a557f4
Iliass JABALI <iliassjabali@gmail.com>
2023-06-21 17:47:23 +0100

    added analytics again

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { api } from "@/utils/api";
 import "@/styles/globals.css";
 import { type AbstractIntlMessages, NextIntlProvider } from "next-intl";
-import {useEffect} from "react";
-import {analytics} from "@/utils/firebase";
+import { useEffect } from "react";
+import Head from "next/head";
+import Script from "next/script";
+import { useRouter } from "next/router";
 
 type PageProps = {
   messages: AbstractIntlMessages;
@@ -23,35 +25,62 @@ const queryClient = new QueryClient({
 
 const gtag = {
   GA_TRACKING_ID: "G-LY2QC1SJ1P",
+  pageview: (url: string) => {
+    // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+    // @ts-ignore
+    window.gtag("config", gtag.GA_TRACKING_ID, {
+      page_path: url,
+    });
+  },
+  event: ({ action, category, label, value }: any) => {
+    // https://developers.google.com/analytics/devguides/collection/gtagjs/events
+    // @ts-ignore
+    window.gtag("event", action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    });
+  },
 };
+
 function MyApp({
   Component,
   pageProps,
 }: Omit<AppProps<PageProps>, "pageProps"> & {
   pageProps: PageProps;
-}) {/*
+}) {
   const router = useRouter();
   useEffect(() => {
     const handleRouteChange = (url: string) => {
-      // @ts-ignore
-      window.gtag("config", gtag.GA_TRACKING_ID, {
-        page_path: url,
-      });
+      gtag.pageview(url);
     };
     router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
       router.events.off("routeChangeComplete", handleRouteChange);
     };
   }, [router.events]);
-
-  */
-  useEffect(() => {
-    analytics;
-  }, []);
   return (
     <NextIntlProvider messages={pageProps.messages}>
       <QueryClientProvider client={queryClient}>
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
 
+                gtag('config', '${gtag.GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+        </Head>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
         <Component {...pageProps} />
         <ReactQueryDevtools />
       </QueryClientProvider>
