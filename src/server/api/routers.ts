b60c96557ba02a626c4c4ca398f6b29f824efe81
Synthwave80 <amine.oussi10@gmail.com>
2023-07-17 13:44:20 +0200

    fix added optional field

--- src/server/api/routers.ts
+++ src/server/api/routers.ts
@@ -1232,7 +1232,7 @@ export const OrderRouter = createTRPCRouter({
       });
     }),
   updateStatus: publicProcedure
-    .input(z.object({ id: z.string(), status: z.nativeEnum(order_status) , paymentId: z.string()}))
+    .input(z.object({ id: z.string(), status: z.nativeEnum(order_status) , paymentId: z.string().optional()}))
     .mutation(async ({ ctx, input }) => {
       return await ctx.prisma.order.update({
         where: {
