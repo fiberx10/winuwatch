7a3564345eb72024849313392a941713e7da621f
Fiberx01 <75884799+fiberx10@users.noreply.github.com>
2023-08-22 01:57:07 +0100

    can't make payment

--- src/pages/Checkout/[id].tsx
+++ src/pages/Checkout/[id].tsx
@@ -320,10 +320,10 @@ export default function CheckoutPage({
       }
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      if (ApplePaySession?.supportsVersion(4) && ApplePaySession?.canMakePayments()) {
+      // if (ApplePaySession?.supportsVersion(4) && ApplePaySession?.canMakePayments()) {
         console.log("Apple Pay is available");
         setupApplepay().catch(console.error);
-      }
+      // }
     }, []);
 
 
